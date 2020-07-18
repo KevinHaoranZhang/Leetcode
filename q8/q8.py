92a36800d4b3c19926ef5c91afcb5e5f0c2cb5e6
kevinzhang <zhr9118@gmail.com>
2020-07-17 21:19:38 -0400

    Solved q8 with Python3

---
+++ q8/q8.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or (x != 0 and x % 10 == 0):
+            return False
+        partial_revert = 0
+        while ( x > partial_revert):
+            partial_revert = int (partial_revert * 10 + x % 10)
+            x = int (x / 10)
+        return x == partial_revert or x == int (partial_revert / 10)

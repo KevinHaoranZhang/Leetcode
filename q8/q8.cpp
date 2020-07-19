64b0b3608085744aef0df57fcee39d3d7a100f43
kevinzhang <zhr9118@gmail.com>
2020-07-19 11:41:42 -0400

    Solved q8 with C++

--- q8/q8.cpp
+++ q8/q8.cpp
@@ -1,14 +1,44 @@
 class Solution {
 public:
-    bool isPalindrome(int x) {
-         if(x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+    int myAtoi(string str) {
+        int i;
+        bool negative = false;
+        for (i = 0; i < str.size(); i++){
+            if (str[i] != ' '){
+                break;
+            }
         }
-        int partial_revert = 0;
-        while(x > partial_revert){
-            partial_revert = partial_revert * 10 + x % 10;
-            x /= 10;
+        if(i == str.size())
+            return 0;
+        if(str[i] == '-' && i+1 != str.size()){
+            negative = true;
+            return findnum (str.substr(i+1, str.size()), negative);
         }
-        return x == partial_revert || x == partial_revert / 10;
+        if (str[i] == '+' && i+1 != str.size())
+            return findnum (str.substr(i+1, str.size()), negative);
+        return findnum (str.substr(i, str.size()), negative);
+        
+    }
+    int findnum (string str, bool negative){
+        if (str.size() == 0)
+            return 0;
+        int num = 0;
+        for(int i = 0; i < str.size(); i++){
+            if( int(str[i]) < 58 && int(str[i]) > 47 ){
+                if(!negative && (num > INT_MAX/10 || (num==INT_MAX/10 && (str[i]-'0')>=7))){
+                    return INT_MAX;
+                }
+                if(negative && (num > INT_MAX/10 || (num==INT_MAX/10 && (str[i]-'0')>=8))){
+                    return INT_MIN;
+                }
+                num = num*10 + (str[i] - '0');
+            }
+            else
+                break;
+        }
+        if(negative)
+            return -num;
+        return num;
+        
     }
 };

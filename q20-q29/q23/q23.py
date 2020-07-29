33649daf020e749451f50ac301473a3b6ddf8d25
kevinzhang <zhr9118@gmail.com>
2020-07-29 14:00:44 -0400

    Solved q23 with Ptyhon3

---
+++ q20-q29/q23/q23.py
@@ -0,0 +1,18 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from Queue import PriorityQueue
+class Solution(object):
+    def mergeKLists(self, lists):
+        dummy = ListNode(None)
+        curr = dummy
+        q = PriorityQueue()
+        for node in lists:
+            if node: q.put((node.val,node))
+        while q.qsize()>0:
+            curr.next = q.get()[1]
+            curr=curr.next
+            if curr.next: q.put((curr.next.val, curr.next))
+        return dummy.next

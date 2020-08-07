55bf217bf7203246821f821dd50ae9367ae5949d
kevinzhang <zhr9118@gmail.com>
2020-08-06 22:23:15 -0400

    Solved q32 with C++

---
+++ q30-q39/q32/q32.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int solution = 0;
+        std::vector <int> dp(s.size());
+        for (int i = 1; i < s.size(); i++){
+            if (s[i] == ')'){
+                if (s[i - 1] == '('){
+                    dp[i] = (i > 2 ? dp[i-2] : 0) + 2;
+                } else if (i - dp[i - 1] > 0 && s[i - dp[i - 1] - 1] == '('){
+                    dp[i] = dp[i - 1] + ((i - dp[i - 1]) >= 2 ? dp[i - dp[i - 1] - 2] : 0) + 2;
+                }
+            }
+        }
+        for (int i = 0; i < dp.size(); i++){
+            if (dp[i] > solution)
+                solution = dp[i];
+        }
+        return solution;
+    }
+};

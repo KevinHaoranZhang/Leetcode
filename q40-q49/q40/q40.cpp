7a8c6a9b934814b0887702612a07a9fd16d735d1
kevinzhang <zhr9118@gmail.com>
2020-08-23 21:01:41 -0400

    Solved q40 with C++

---
+++ q40-q49/q40/q40.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        std::vector<int> path;
+        std::vector<std::vector<int>> solution;
+        std::sort(candidates.begin(), candidates.end());
+        dfs(candidates, target, 0, path, solution);
+        return solution;
+    }
+private:
+    void dfs(vector<int>& candidates, int target, int index, vector<int>& path, vector<vector<int>>& solution){
+        if (target == 0){
+            solution.push_back(path);
+            return;
+        }
+        for (int i = index; i < candidates.size(); i++){
+            if (candidates[i] > target)
+                return;
+            path.push_back(candidates[i]);
+            dfs(candidates, target-candidates[i], i+1, path, solution);
+            path.pop_back();
+            while(i < candidates.size()-1 && candidates[i]==candidates[i+1])
+                i++;
+        }
+    }
+};

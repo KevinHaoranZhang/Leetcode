2f57a6034d22789225b137a30977b12bfb306f2e
kevinzhang <zhr9118@gmail.com>
2020-08-27 10:34:13 -0400

    Solved q45 with C++

---
+++ q40-q49/q45/q45.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int res = 0, n = nums.size(), i = 0, cur = 0;
+        while (cur < n - 1) {
+            ++res;
+            int pre = cur;
+            for (; i <= pre; ++i) {
+                cur = max(cur, i + nums[i]);
+            }
+        }
+        return res;
+    }
+};
